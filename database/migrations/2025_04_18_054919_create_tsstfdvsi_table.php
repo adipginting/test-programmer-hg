11210cd476203abe038703b07fe994a3d841efc8
Adi Primanda Ginting <adi@adipginting.com>
2025-04-18 13:04:48 +0700

    tssfdvsi migration

---
+++ database/migrations/2025_04_18_054919_create_tsstfdvsi_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tsstfdvsi', function (Blueprint $table) {
+            $table->char('kddvsi', 2)->primary();
+            $table->char('nama', 100);
+            $table->double('dvsic', 3, 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tsstfdvsi');
+    }
+};
